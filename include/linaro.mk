a68c157ac2e9f9100e968efa6dbfb08e9e7af55f
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-03-17 02:15:51 +0000

    Do not define LIBSIMPLEIO anymore.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@19360 06ee2f96-5407-11de-8bab-f3d7589a4122

--- include/linaro.mk
+++ include/linaro.mk
@@ -1,6 +1,6 @@
 # Global definitions for cross-toolchains built with Linaro ABE
 
-# Copyright (C)2013-2020, Philip Munts, President, Munts AM Corp.
+# Copyright (C)2013-2021, Philip Munts, President, Munts AM Corp.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -42,7 +42,3 @@ FPC		= /usr/local/fpc-$(TOOLCHAIN_NAME)/bin/$(FPC_COMMAND)
 
 GNATPREFIX	= $(CROSS_COMPILE)
 GPRBUILDCONFIG	= --config=$(TOOLCHAIN_DIR)/share/gpr/$(CONFIGURE_NAME).cgpr
-
-# libsimpleio
-
-LIBSIMPLEIO	?= $(GCCSYSROOT)/usr/share/libsimpleio
