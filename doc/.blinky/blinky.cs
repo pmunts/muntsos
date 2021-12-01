dcfaf8c6a2497b7c8f317c0d7fe24bad57bc0607
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-12-01 05:31:43 +0000

    Simplified to a top-level program.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@20142 06ee2f96-5407-11de-8bab-f3d7589a4122

--- doc/.blinky/blinky.cs
+++ doc/.blinky/blinky.cs
@@ -1,31 +1,22 @@
-using System;
+using static System.Console;
 
-namespace blinky
+WriteLine("\nMuntsOS C# LED Test\n");
+
+// Configure a GPIO output to drive an LED
+
+IO.Objects.libsimpleio.Device.Designator desg_LED =
+  new IO.Objects.libsimpleio.Device.Designator(0, 26);
+
+IO.Interfaces.GPIO.Pin LED =
+  new IO.Objects.libsimpleio.GPIO.Pin(desg_LED,
+    IO.Interfaces.GPIO.Direction.Output);
+
+// Flash the LED forever (until killed)
+
+WriteLine("Press CONTROL-C to exit.\n");
+
+for (;;)
 {
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("\nMuntsOS C# LED Test\n");
-
-      // Configure a GPIO output to drive an LED
-
-      IO.Objects.libsimpleio.Device.Designator desg_LED =
-        new IO.Objects.libsimpleio.Device.Designator(0, 26);
-
-      IO.Interfaces.GPIO.Pin LED =
-        new IO.Objects.libsimpleio.GPIO.Pin(desg_LED,
-          IO.Interfaces.GPIO.Direction.Output);
-
-      // Flash the LED forever (until killed)
-
-      Console.WriteLine("Press CONTROL-C to exit.\n");
-
-      for (;;)
-      {
-        LED.state = !LED.state;
-        System.Threading.Thread.Sleep(500);
-      }
-    }
-  }
+  LED.state = !LED.state;
+  System.Threading.Thread.Sleep(500);
 }
