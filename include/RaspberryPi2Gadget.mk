b800eba9cfe726e9ae7d7f9bfa6f237956cc7bbc
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2022-08-21 23:19:15 +0000

    Device tree blobs for Raspberry Pi 32-bit targets are in dtbs/, not dtbs/broadcom/
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@20406 06ee2f96-5407-11de-8bab-f3d7589a4122

--- include/RaspberryPi2Gadget.mk
+++ include/RaspberryPi2Gadget.mk
@@ -32,9 +32,9 @@ else
 BOARDBASE	:= RaspberryPi2
 endif
 
-KERNEL_DTB	+= broadcom/bcm2710-rpi-3-b-plus
-KERNEL_DTB	+= broadcom/bcm2710-rpi-cm3
-KERNEL_DTB	+= broadcom/bcm2710-rpi-zero-2
+KERNEL_DTB	+= bcm2710-rpi-3-b-plus
+KERNEL_DTB	+= bcm2710-rpi-cm3
+KERNEL_DTB	+= bcm2710-rpi-zero-2
 
 KERNEL_OVL	+= dwc2
 
