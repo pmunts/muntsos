76a0bdef26ee5e52578cf13193f8b19938bd2f18
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-09-08 08:38:30 +0000

    Started working on support for the LePotato.
    https://libre.computer/products/aml-s905x-cc
    
    
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@21323 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ include/LePotato.mk
@@ -0,0 +1,54 @@
+# Global make definitions for the BeagleBone ARM Linux microcomputer
+
+# Copyright (C)2013-2023, Philip Munts dba Munts Technologies.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+PLATFORM_NAME	= lepotato
+
+TOOLCHAIN_BUILDER ?= crosstool
+
+include $(MUNTSOS)/include/AArch64.mk
+include $(MUNTSOS)/include/$(TOOLCHAIN_BUILDER).mk
+
+BOARDBASE	:= LePotato
+
+KERNEL_IMGSRC	= Image
+KERNEL_IMG	= $(BOARDBASE).img
+KERNEL_TARGETS	= $(KERNEL_IMGSRC) dtbs
+
+LOADER		= ld-linux-aarch64.so.1
+
+BOOTFILESDIR	= $(MUNTSOS)/boot/$(BOARDBASE)
+BOOTKERNELDIR	= $(MUNTSOS)/bootkernel
+BOOTKERNELTGZ	= $(BOOTKERNELDIR)/$(BOARDNAME)-Kernel.tgz
+
+# Definitions for the Beagleboard kernel repository
+
+KERNEL_REPO     = https://github.com/libre-computer-project/libretech-linux.git
+KERNEL_BRANCH	= 6.1
+KERNEL_TREEISH	= linux-$(KERNEL_BRANCH).y-lc
+KERNEL_NAME	= linux-$(PLATFORM_NAME)-$(KERNEL_BRANCH)
+KERNEL_CLONE	= $(TEMP)/$(KERNEL_NAME)
+KERNEL_DIST	= $(TEMP)/$(KERNEL_NAME).tgz
+KERNEL_COMMIT	= $(TEMP)/$(KERNEL_NAME).commit
+
+CFLAGS		+= -DLEPOTATO
+
+include $(MUNTSOS)/include/common.mk
