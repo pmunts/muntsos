3b852a00bdd87befcf522a953a6140f94f32f65d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2024-01-11 16:24:55 +0000

    Added CM4-Duino LED test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@21764 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ examples/ada/programs/test_cm4_duino_led.adb
@@ -0,0 +1,44 @@
+-- Waveshare CM4-Duino (https://www.waveshare.com/wiki/CM4-Duino) LED Test
+
+-- Copyright (C)2024, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO;
+USE Ada.Text_IO;
+
+WITH Arduino.CM4_Duino;
+WITH GPIO;
+
+PROCEDURE test_cm4_duino_led IS
+
+  LED : GPIO.Pin;
+
+BEGIN
+  New_Line;
+  Put_Line("CM4-Duino LED Test");
+  New_Line;
+
+  LED := Arduino.CM4_Duino.Create(Arduino.CM4_Duino.LED);
+
+  LOOP
+    LED.Put(NOT LED.Get);
+    DELAY 0.5;
+  END LOOP;
+END test_cm4_duino_led;
