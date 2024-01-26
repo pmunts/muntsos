a57ba72418d7f11256a234ca576541bd275663ab
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2024-01-26 01:11:01 +0000

    Added device tree overlays pcf857x and pca953x.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@21924 06ee2f96-5407-11de-8bab-f3d7589a4122

--- include/RaspberryPi3.mk
+++ include/RaspberryPi3.mk
@@ -37,6 +37,7 @@ KERNEL_DTB	+= broadcom/bcm2710-rpi-zero-2
 KERNEL_OVL	+= overlay_map
 KERNEL_OVL	+= disable-bt disable-wifi dwc2
 KERNEL_OVL	+= anyspi i2c-rtc pwm pwm-2chan
+KERNEL_OVL	+= pcf857x pca953x
 KERNEL_OVL	+= spi1-1cs spi1-2cs spi1-3cs
 KERNEL_OVL	+= spi1-2cs spi2-2cs spi2-3cs
 
