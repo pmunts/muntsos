5498edd54e50d45437f662717be589c8977ba7b6
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-06-30 14:45:09 +0000

    Renamed get_messages to wioe5_ham1_nng_messages
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@23424 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ examples/ada/programs/wioe5_ham1_nng_messages.adb
@@ -0,0 +1,41 @@
+-- Display messages from wioe5_ham1_nng_publisher
+
+-- Copyright (C)2025, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH NNG.Sub;
+
+PROCEDURE wioe5_ham1_nng_messages IS
+
+  client : NNG.Sub.Client;
+
+BEGIN
+  New_Line;
+  Put_Line("Display messages from wioe5_ham1_nng_publisher");
+  New_Line;
+
+  client.Initialize("ipc:///tmp/wioe5.sock");
+
+  LOOP
+    Put_Line(client.Get);
+  END LOOP;
+END wioe5_ham1_nng_messages;
