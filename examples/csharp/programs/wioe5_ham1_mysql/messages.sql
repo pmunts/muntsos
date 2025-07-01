eded739a3644850f751346b196db5775e6facd57
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-07-01 16:39:04 +0000

    Autoinitialize the timestamp
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@23435 06ee2f96-5407-11de-8bab-f3d7589a4122

--- examples/csharp/programs/wioe5_ham1_mysql/messages.sql
+++ examples/csharp/programs/wioe5_ham1_mysql/messages.sql
@@ -2,10 +2,10 @@ drop table if exists LoRaMessages;
 
 create table LoRaMessages
 (
-  time     datetime,
   sender   varchar(20),
   receiver varchar(20),
   RSS      int,
   SNR      int,
   message  varchar(256)
+  time     datetime default current_timestamp,
 );
