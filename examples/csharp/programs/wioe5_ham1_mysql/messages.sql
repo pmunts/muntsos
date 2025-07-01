bd9f468b6d891350aa189f330aef801cfbf96fde
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-07-01 17:20:02 +0000

    Removed an extra space
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@23437 06ee2f96-5407-11de-8bab-f3d7589a4122

--- examples/csharp/programs/wioe5_ham1_mysql/messages.sql
+++ examples/csharp/programs/wioe5_ham1_mysql/messages.sql
@@ -2,10 +2,10 @@ drop table if exists LoRaMessages;
 
 create table LoRaMessages
 (
-  sender   varchar(20),
-  receiver varchar(20),
-  RSS      int,
-  SNR      int,
-  message  varchar(256),
-  time     datetime default current_timestamp
+  sender   varchar(20) not null,
+  receiver varchar(20) not null,
+  RSS      int not null,
+  SNR      int not null,
+  message  varchar(256) not null,
+  time     datetime not null default current_timestamp
 );
