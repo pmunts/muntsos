1ef730fd6ebed8076144e5992bf91099e3f1e6df
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-07-14 01:46:10 +0000

    Added Ada extension program template.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@23501 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ examples/ada/programs/extension_program_template.adb
@@ -0,0 +1,48 @@
+-- MuntsOS Embedded Linux Ada Program Extension Template
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH Debug;
+WITH libLinux;
+WITH Watchdog.libsimpleio;
+
+PROCEDURE extension_program_template IS
+
+  err      : Integer;
+  wd       : Watchdog.Timer;
+
+  -- Declare your application specific variables here
+
+BEGIN
+  IF Debug.Enabled THEN
+    New_Line;
+    Put_Line("MuntsOS Embedded Linux Ada Program Extension Template");
+    New_Line;
+  ELSE
+    New_Line;
+    Put("Wio-E5 LoRa Transceiver NNG Message Publisher");
+
+    -- Run as background process
+
+    libLinux.Detach(err);
+
+    -- Create a watchdog timer device object
+
+    wd := Watchdog.libsimpleio.Create;
+    wd.SetTimeout(5.0);
+
+    -- Any unhandled exception or signal after this point cause the target
+    -- computer to reset.
+  END IF;
+
+  -- Add your application specific initialization code here
+
+  LOOP
+
+    -- Add your application specific event handling code here
+
+    IF NOT Debug.Enabled THEN
+      wd.Kick;
+    END IF;
+  END LOOP;
+END extension_program_template;
