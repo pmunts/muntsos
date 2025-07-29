857cbe63f11405461bfeb8e8800709e732b19ce1
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-07-29 02:28:03 +0000

    More progress
    Invoke loader from post-install script
    
    
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@23529 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ extensions/buildhat-firmware/loader.adb
@@ -0,0 +1,31 @@
+-- Copyright (C)2025, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+PROCEDURE Loader IS
+
+  Firmware_DirName   : CONSTANT String := "/lib/firmware/buildhat";
+  Firmware_FileName  : CONSTANT String := Firmware_DirName & "/firmware.bin";
+  Signature_Filename : CONSTANT String := Firmware_DirName & "/signature.bin";
+
+BEGIN
+  Put_Line("Raspberry Pi Build HAT Firmware Loader");
+END Loader;
