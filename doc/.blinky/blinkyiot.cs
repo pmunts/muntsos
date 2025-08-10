4a60616c1d10f95e40b3e048dbe9e321ef25bc1d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-08-10 21:06:59 +0000

    Fleshed out the program
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@23574 06ee2f96-5407-11de-8bab-f3d7589a4122

--- doc/.blinky/blinkyiot.cs
+++ doc/.blinky/blinkyiot.cs
@@ -1,5 +1,20 @@
 using System.Device.Gpio;
 
 using static System.Console;
+using static System.Threading.Thread;
 
 WriteLine("\nSystem.Device.Gpio LED Test\n");
+
+var dev = new GpioController();
+
+const int LED = 26;
+
+dev.OpenPin(LED, PinMode.Output);
+
+for (;;)
+{
+    dev.Write(LED, true);
+    Thread.Sleep(500);
+    dev.Write(LED, false);
+    Thread.Sleep(500);
+}
