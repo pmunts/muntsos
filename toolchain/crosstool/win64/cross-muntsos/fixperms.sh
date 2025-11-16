aea0372c5528f2d50aa32bec2045adb3203a7a05
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-11-16 08:43:01 +0000

    Don't need to patch fixperms.sh anymore.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@24012 06ee2f96-5407-11de-8bab-f3d7589a4122

--- toolchain/crosstool/win64/cross-muntsos/fixperms.sh
+++ toolchain/crosstool/win64/cross-muntsos/fixperms.sh
@@ -2,8 +2,8 @@
 
 # Fix permissions after installing from tarball with Cygwin tar
 
-find @@TOOLCHAIN_DIR@@ -type f -exec chmod 444 {} ";"
-find @@TOOLCHAIN_DIR@@ -type f -name '*.dll' -exec chmod 555 {} ";"
-find @@TOOLCHAIN_DIR@@ -type f -name '*.exe' -exec chmod 555 {} ";"
-find @@TOOLCHAIN_DIR@@ -type f -name fixperms.sh -exec chmod 555 {} ";"
-find @@TOOLCHAIN_DIR@@ -type d -exec chmod 555 {} ";"
+/bin/find * -type f                   -print -exec chmod 444 {} ";"
+/bin/find * -type f -name '*.dll'     -print -exec chmod 555 {} ";"
+/bin/find * -type f -name '*.exe'     -print -exec chmod 555 {} ";"
+/bin/find * -type f -name fixperms.sh -print -exec chmod 555 {} ";"
+/bin/find * -type d                   -print -exec chmod 555 {} ";"
