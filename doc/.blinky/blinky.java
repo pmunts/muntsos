c921cfae2b641bc65c8dec6f8106379b3528d31d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-01-26 05:47:41 +0000

    Added blinky.java and Makefile.java.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/muntsos@24235 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ doc/.blinky/blinky.java
@@ -0,0 +1,27 @@
+import com.munts.interfaces.GPIO.*;
+import com.munts.libsimpleio.objects.GPIO.*;
+
+public class blinky
+{
+  public static void main(String args[])
+    throws InterruptedException
+  {
+    System.out.println("\nMuntsOS Java LED Test\n");
+
+    // Configure a GPIO output to drive an LED
+
+    Builder b = new Builder(0, 26);
+    b.SetDirection(Direction.Output);
+    Pin LED = b.Create();
+
+    // Flash the LED forever (until killed)
+
+    System.out.println("Press CONTROL-C to exit.\n");
+
+    for (;;)
+    {
+      LED.write(!LED.read());
+      Thread.sleep(500);
+    }
+  }
+}
